fc01e93f7be201de9be2f4308181d3437a08d69a
Thongzxje <thonghoangdanh@gmail.com>
2022-10-27 01:38:16 +0300

    implementing top charts, top artists

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,12 +7,12 @@ import { logo } from "../assets";
 import { links } from "../assets/constants";
 
 const NavLinks = ({ handleClick }) => (
-  <div className="mt-10">
+  <div className="absolute mt-10">
     {links.map((item) => (
       <NavLink
         key={item.name}
         to={item.to}
-        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-[#CFB997]"
         onClick={() => handleClick && handleClick()}
       >
         <item.icon className="w-6 h-6 mr-2" />
@@ -27,8 +27,8 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-green">
+        {/* <img src={logo} alt="logo" className="w-full h-14 object-contain" /> */}
         <NavLinks />
       </div>
       <div className="absolute md:hidden block top-6 right-3">
@@ -36,9 +36,9 @@ const Sidebar = () => {
           <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)} />
         ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)} />}
       </div>
-      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-green backdrop-blur-lg p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-        <NavLinks onClick={() => setMobileMenuOpen(false)} />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        black: '#191624',
+        green: '#395144',
       },
       animation: {
         slideup: 'slideup 1s ease-in-out',
