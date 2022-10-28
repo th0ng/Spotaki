f5fde386cb8a4e0821abbd7ea732684526a087fa
Thongzxje <thonghoangdanh@gmail.com>
2022-10-28 22:15:43 +0300

    Related Songs fixed

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -4,28 +4,30 @@ const RelatedSongs = ({
   data,
   isPlaying,
   activeSong,
+  artistId,
   handlePauseClick,
   handlePlayClick,
-  artistId,
-}) => (
-  <div className="flex flex-col">
-    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+}) => {
+  return (
+    <div className="flex flex-col">
+      <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
 
-    <div className="mt-6 w-full flex flex-col">
-      {data?.map((song, i) => (
-        <SongBar 
-          key={`${song.key}-${artistId}`}
-          song={song}
-          i={i}
-          artistId={artistId}
-          isPlaying={isPlaying}
-          activeSong={activeSong}
-          handlePauseClick={handlePauseClick}
-          handlePlayClick={handlePlayClick}
-        />
-      ))}
+      <div className="mt-6 w-full flex flex-col">
+        {data?.map((song, i) => (
+          <SongBar 
+            key={`${song.key}-${artistId}`}
+            song={song}
+            i={i}
+            artistId={artistId}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  ) 
+};
 
 export default RelatedSongs;
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -2,8 +2,6 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
-
-
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
 
 import { useGetSongDetailsQuery, useGetSongRelatedQuery } from "../redux/services/shazamCore";
@@ -19,8 +17,8 @@ const SongDetails = () => {
   const handlePauseClick = () => {
     dispatch(playPause(false));
   };
-  const handlePlayClick = (song, i) => {
-    dispatch(setActiveSong({ song, i }));
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, relatedSongsData, i }));
     dispatch(playPause(true));
   };
 
