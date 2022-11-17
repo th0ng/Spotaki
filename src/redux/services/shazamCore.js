ae1779f0483a042ae82ab50e2aa1d2ff43356c26
Thongzxje <thonghoangdanh@gmail.com>
2022-11-18 00:37:41 +0200

    update key

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,23 +1,39 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const shazamCoreApi = createApi({
-  reducerPath: 'shazamCoreApi',
+  reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', '6f745ae989mshc3f98f5c98f1d3ep15db57jsn5fcc56fae5ad');
+      headers.set(
+        "X-RapidAPI-Key",
+        "058a813a87mshbe656e5a5adace6p16735ajsnc2fe5f147d31"
+      );
 
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => '/charts/world' }),
-    getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
-    getSongDetails: builder.query({ query: (songid) => `/tracks/details?track_id=${songid}` }),
-    getSongRelated: builder.query({ query: (songid) => `/tracks/related?track_id=${songid}` }),
-    getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
-    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
-    getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/charts/genre-world?genre_code=${genre}`,
+    }),
+    getSongDetails: builder.query({
+      query: (songid) => `/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: (songid) => `/tracks/related?track_id=${songid}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `/artists/details?artist_id=${artistId}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `/charts/country?country_code=${countryCode}`,
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
   }),
 });
 
